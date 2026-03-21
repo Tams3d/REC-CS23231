a5493f900560aa939260f35f3fd299afe0fdb62d
Tamil Selvan <106262964+Tams3d@users.noreply.github.com>
2026-03-21 22:18:49 +0530

    LinkedList: Extra Functions

---
+++ LinkedList/extra/reverse_list.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void print_list(struct Node *head) {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void insert_at_end(struct Node **head, int data) {
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+
+    if (new_node == NULL) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+
+    new_node->data = data;
+    new_node->next = NULL;
+
+    if (*head == NULL) {
+        *head = new_node;
+        return;
+    }
+
+    struct Node *temp = *head;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->next = new_node;
+}
+
+void reverse_list(struct Node **head) {
+    if (*head == NULL || (*head)->next == NULL) {
+        return;
+    }
+    struct Node *prev = NULL;
+    struct Node *next = NULL;
+    struct Node *curr = *head;
+
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    *head = prev;
+}
+
+int main() {
+    struct Node *head = NULL;
+
+    insert_at_end(&head, 1);
+    insert_at_end(&head, 2);
+    insert_at_end(&head, 3);
+    print_list(head);
+
+    reverse_list(&head);
+    print_list(head);
+}
