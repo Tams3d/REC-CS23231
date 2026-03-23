bfe6bb28ef27a2e538dba2da38a228f03297a577
Tamil Selvan <106262964+Tams3d@users.noreply.github.com>
2026-03-23 15:13:50 +0530

    LinkedList: Postclass

---
+++ LinkedList/postclass/postclass_02_remove_last_book_library_catalog.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *prev;
+    struct Node *next;
+};
+
+void print_list(struct Node *head) {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+    struct Node *temp = head;
+    while (temp != NULL) {
+        printf("%d", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void insert_at_end(struct Node **head, int data) {
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    if (new_node == NULL) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    new_node->data = data;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+
+    if (*head == NULL) {
+        *head = new_node;
+        return;
+    }
+
+    struct Node *temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = new_node;
+    new_node->prev = temp;
+}
+
+void delete_last(struct Node **head) {
+    if (*head == NULL)
+        return;
+
+    if ((*head)->next == NULL) {
+        free(*head);
+        *head = NULL;
+        return;
+    }
+
+    struct Node *temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->prev->next = NULL;
+    free(temp);
+}
+
+int main() {
+    struct Node *head = NULL;
+
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int data;
+        scanf("%d", &data);
+        insert_at_end(&head, data);
+    }
+
+    delete_last(&head);
+    print_list(head);
+
+    return 0;
+}
---
+++ LinkedList/postclass/postclass_03_circular_todo_insert_end.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void print_list(struct Node *head) {
+    if (head == NULL)
+        return;
+    struct Node *temp = head;
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("\n");
+}
+
+void insert_at_end(struct Node **head, int data) {
+    struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
+    if (new_node == NULL) {
+        printf("Memory allocation failed\n");
+        return;
+    }
+    new_node->data = data;
+
+    if (*head == NULL) {
+        new_node->next = new_node;
+        *head = new_node;
+        return;
+    }
+
+    struct Node *temp = *head;
+    while (temp->next != *head)
+        temp = temp->next;
+
+    temp->next = new_node;
+    new_node->next = *head;
+}
+
+int main() {
+    struct Node *head = NULL;
+
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        int data;
+        scanf("%d", &data);
+        insert_at_end(&head, data);
+    }
+
+    print_list(head);
+
+    return 0;
+}
